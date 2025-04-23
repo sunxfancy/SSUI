28b47dce221ae4f41ee3af5d8343787ceebb2520
西风逍遥游 <sunxfancy@gmail.com>
2025-04-22 18:26:12 -0700

    update index

--- frontend/ssui_components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './dist/ComponentsManager';
-export * from './dist/Message';
-export * from './dist/controlers/InternalControlers';
-export * from './dist/components/Base/PromptEditor';
-export * from './dist/components/Loader/SDLoader';
-export * from './dist/components/ComponentRef';
-export * from './dist/controlers/ControlerRef';
