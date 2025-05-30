5f6c5466d2b62a7d1ec550587699673fcc2b04ad
西风逍遥游 <sunxfancy@gmail.com>
2025-05-29 21:26:25 -0700

    fix string editor

--- frontend/ssui_components/src/components/Base/StringEditor.tsx
+++ frontend/ssui_components/src/components/Base/StringEditor.tsx
@@ -4,7 +4,7 @@ import styles from './test.module.css'
 
 export class StringEditor extends IComponent<{}, { textContent: string }> {
     render(): ReactNode {
-        return <Text className={styles.normal}>String Editor</Text>
+        return <input type="text" className={styles.normal}></input>
     }
 
     onExecute(): any {
@@ -22,6 +22,6 @@ import { ReactNode } from 'react';
 // Register into the component manager
 import { registerComponent, ComponentRegister } from '../ComponentsManager';
 [
-    { 'name': 'StringEditor', 'type': 'string', 'port': 'input', 'component': StringEditor } as ComponentRegister,
-    { 'name': 'StringPreview', 'type': 'string', 'port': 'output', 'component': StringPreview } as ComponentRegister
+    { 'name': 'StringEditor', 'type': 'builtins.str', 'port': 'input', 'component': StringEditor } as ComponentRegister,
+    { 'name': 'StringPreview', 'type': 'builtins.str', 'port': 'output', 'component': StringPreview } as ComponentRegister
 ].forEach(registerComponent);
